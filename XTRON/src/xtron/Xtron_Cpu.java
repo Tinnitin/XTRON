a8d282a41c150c24e9e2a3762fe076f7f27c4050
Agustin Camacho <tinnit85@gmail.com>
2015-10-11 15:40:20 -0600

    cpu

--- XTRON/src/xtron/Xtron_Cpu.java
+++ XTRON/src/xtron/Xtron_Cpu.java
@@ -12,131 +12,177 @@ import javax.swing.JOptionPane;
  * @author Tin
  */
 public class Xtron_Cpu {
- String [] memory;// contiene la memoria del Xtron 
- int instructionCounter;//pocision actual de la instruccion 
- String instructionRegister;//pocision de la siguiente instruccion 
- String operateCode;// indica la operacion a realizar
- String operand;// donde guardo la operacion
- int acumulator;
+
+    String[] memory;// contiene la memoria del Xtron 
+    int instructionCounter;//pocision actual de la instruccion 
+    String instructionRegister;//pocision de la siguiente instruccion 
+    String operateCode;// indica la operacion a realizar
+    String operand;// donde guardo la operacion
+    int acumulator;
+
     public Xtron_Cpu() {
-       this.memory = new String[99];
-       this.acumulator=0;
-       this.instructionCounter=0;
-       this.instructionRegister="0000";
-       this.operateCode="00";
-       this.operand="00";   
-      
+        this.memory = new String[99];
+        this.acumulator = 0;
+        this.instructionCounter = 0;
+        this.instructionRegister = "0000";
+        this.operateCode = "00";
+        this.operand = "00";
+
     }
-    public void CPU(String []program){
-        
-    boolean validProgram=validateProgram(program);
-    if(validProgram==true){//el programa es valido
-       chargeMemory(program);
-       
-        for (int i = 0; i <program.length; i++) {
-            
-           instructionRegister=memory[instructionCounter];
-           operateCode=instructionRegister.substring(1, 3);
-           operand=instructionRegister.substring(3,5);
-           
-           switch(operateCode)
-        {
-            case "10"://"READ una variable y la guarda en operand";
+
+    public String[] CPU(String[] program) {
+
+        boolean validProgram = validateProgram(program);
+        if (validProgram == true) {//el programa es valido
+            chargeMemory(program);
+
+            for (int i = 0; i < program.length-1; i++) {
+
+                instructionRegister = memory[instructionCounter];
+                operateCode = instructionRegister.substring(0, 2);
+                operand = instructionRegister.substring(2, 4);
+
+                switch (operateCode) {
+                    case "10"://Read lee una palabra desde el teclado y la guarda en una pocision espesifica
+  
+                        break;
+                    case "11":
+                        //  "WRITE";toma una palabra de una ubicacion espesifica y la imprime en pantalla
+                        break;
+                    case "20":
+                        // "LOAD";toma una palabra de memoria y la colca en el acumulador 
+                        if (Integer.parseInt(operand) > program.length) {
+
+                            acumulator = Integer.parseInt(memory[Integer.parseInt(operand)]);
+                        } else {
+                            ///error de compilacion
+                        }
+
+                        break;
+                    case "21":
+                        // "STORE";almacena el acumulador en una pocision de memoria
+                        if (Integer.parseInt(operand) > program.length) {
+
+                            memory[Integer.parseInt(operand)] = acumulator + "";
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "30":
+                        //"ADD";suma una pocision de memoria al acumulador y mantiene la suma ahi
+                        if (Integer.parseInt(operand) > program.length) {
+
+                            acumulator = acumulator + Integer.parseInt(memory[Integer.parseInt(operand)]);
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "31":
+                        // "SUBTRACT";resta una poscision de memoria al acumulador 
+                        if (Integer.parseInt(operand) > program.length) {
+
+                            acumulator = acumulator - Integer.parseInt(memory[Integer.parseInt(operand)]);
+
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "32":
+                        // "DIBIDE";
+                        if (Integer.parseInt(operand) > program.length) {
+
+                            acumulator = acumulator / Integer.parseInt(memory[Integer.parseInt(operand)]);
+
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "33":
+                        //"MULTIPLY";
+                         if(Integer.parseInt(operand)<program.length){
                
-                
-                break;
-            case "11":
-              //  "WRITE";
-                break;
-            case "20":
-               // "LOAD";
-            if(Integer.parseInt(operand)>program.length){
-          
-                acumulator= Integer.parseInt(memory[Integer.parseInt(operand)])  ;
-            }
-            else{
-            ///error de compilacion
-            }
-                
-                break;
-            case "21":
-               // "STORE";
-                if(Integer.parseInt(operand)>program.length){
-          
-                memory[Integer.parseInt(operand)]=acumulator+"";
-            }
-                else{
-                    
-                 ///error de compilacion
-               }
-                 break;
-            case "30":
-                //"ADD";
-                   if(Integer.parseInt(operand)>program.length){
-          
-                   acumulator= acumulator+Integer.parseInt(memory[Integer.parseInt(operand)]);
-            }
-                else{
-                    
-                 ///error de compilacion
-               }
-                break;
-            case "31":
-             // "SUBTRACT";
-                if(Integer.parseInt(operand)>program.length){
-          
+                         acumulator=acumulator*Integer.parseInt(memory[Integer.parseInt(operand)]);
                   
             }
                 else{
                     
                  ///error de compilacion
                }
-                break;
-            case "32":
-               // "DIBIDE";
-                break;
-            case "33":
-                //"MULTIPLY";
-                break;
-            case "40":
-               // "BRANCH";
-                break;
-            case "41":
-               // "BRANCHNEG";
-                break;
-            case "42":
-               //"BRANCHZERO";
-                break;
-            case "43":
-           // "HALT";
-                break;
-            default:
-               
-                break;
+                        break;
+                    case "40":
+                        // "BRANCH";
+                        if (Integer.parseInt(operand) < program.length) {
+
+                            instructionCounter=Integer.parseInt(operand);
+                            i=Integer.parseInt(operand);
+
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "41":
+                        // "BRANCHNEG";
+                        if (Integer.parseInt(operand) < program.length ) {
+                            if(acumulator>=0){
+                            instructionCounter=Integer.parseInt(operand);
+                            i=Integer.parseInt(operand);
+                            }
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "42":
+                        //"BRANCHZERO";
+                         if (Integer.parseInt(operand) < program.length ) {
+                            if(acumulator!=0){
+                            instructionCounter=Integer.parseInt(operand);
+                            i=Integer.parseInt(operand);
+                            }
+                        } else {
+
+                            ///error de compilacion
+                        }
+                        break;
+                    case "43":
+                        // "HALT";finaliza  el programa
+                        return memory;
+                     
+                    default:
+
+                        break;
+                }
+
+            }
+            return memory;
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Program Invalid");
+
         }
-            
-        }
-       
+        return memory;
+
     }
-    else{
-        JOptionPane.showMessageDialog(null,"Program Invalid");
-    
-    }
-    
-    }
+
     //
-    public boolean validateProgram(String []program){
-    
-    
+
+    public boolean validateProgram(String[] program) {
+
         //hoadslfjadslfalskfjaslfdsñf
         return true;
     }
+
     //carga la memoria con el nuevo programa 
-    private void  chargeMemory(String[] program) {
+
+    private void chargeMemory(String[] program) {
         for (int i = 0; i < program.length; i++) {
-            memory[i]=program[i];
+            memory[i] = program[i];
         }
     }
-   
-    
+
 }
